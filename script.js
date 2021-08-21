9067286ddcef24dc392cb5387b9982653b85ec3f
Godiluck <givan3819@gmail.com>
2021-08-22 00:44:25 +0300

    .

---
+++ script.js
@@ -0,0 +1,13 @@
+
+       new Swiper(".slider", {
+        cssMode: true,
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+        mousewheel: true,
+        keyboard: true,
+      });
+
+
+    
